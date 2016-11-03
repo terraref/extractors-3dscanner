64176f871a5c09da50211a8486930baaae318478
Max Burnette <mburnet88@gmail.com>
2016-11-03 07:55:37 -0500

    Update batch_launcher path

--- ply2las/batch_launcher.sh
+++ ply2las/batch_launcher.sh
@@ -6,7 +6,7 @@ module purge
 module load python/2.7.10 pythonlibs/2.7.10 pdal
 
 # Activate python virtualenv
-source /home/mburnet2/extractors/pyenv/bin/activate
+source /projects/arpae/terraref/shared/extractors/pyenv/bin/activate
 
 # Run extractor script
-python /home/mburnet2/extractors/extractors-3dscanner/ply2las/terra.ply2las.py
+python /projects/arpae/terraref/shared/extractors/extractors-3dscanner/ply2las/terra.ply2las.py
