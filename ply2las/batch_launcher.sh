64533cd75306025fd92895ea93d12a02884d8673
Max Burnette <mburnet88@gmail.com>
2016-11-08 09:53:03 -0600

    Add modules to batch launcher

--- ply2las/batch_launcher.sh
+++ ply2las/batch_launcher.sh
@@ -3,7 +3,7 @@
 
 # Load necessary modules
 module purge
-module load python/2.7.10 pythonlibs/2.7.10 pdal
+module load python/2.7.10 pythonlibs/2.7.10 GCC proj4 pdal
 
 # Activate python virtualenv
 source /projects/arpae/terraref/shared/extractors/pyenv/bin/activate
