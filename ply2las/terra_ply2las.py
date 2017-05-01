2b23e32f72be9ba5248874478c23ae5e69b54f6e
Max Burnette <mburnet88@gmail.com>
2017-05-01 11:19:45 -0500

    Update ply2las

--- ply2las/terra_ply2las.py
+++ ply2las/terra_ply2las.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python
 
-"""
-terra.ply2las.py
-
-This extractor will trigger when a PLY file is uploaded into Clowder. It will create an LAS file from the PLY file.
-"""
-
 import os
 import logging
 import subprocess
 import tempfile
+
+import datetime
+from dateutil.parser import parse
+from influxdb import InfluxDBClient, SeriesHelper
 
 from pyclowder.extractors import Extractor
 from pyclowder.utils import CheckMessage
@@ -21,12 +19,40 @@ class Ply2LasConverter(Extractor):
     def __init__(self):
         Extractor.__init__(self)
 
+        # add any additional arguments to parser
+        # self.parser.add_argument('--max', '-m', type=int, nargs='?', default=-1,
+        #                          help='maximum number (default=-1)')
+        self.parser.add_argument('--output', '-o', dest="output_dir", type=str, nargs='?',
+                                 default="/home/extractor/sites/ua-mac/Level_1/flirIrCamera",
+                                 help="root directory where timestamp & output directories will be created")
+        self.parser.add_argument('--overwrite', dest="force_overwrite", type=bool, nargs='?', default=False,
+                                 help="whether to overwrite output file if it already exists in output directory")
+        self.parser.add_argument('--influxHost', dest="influx_host", type=str, nargs='?',
+                                 default="terra-logging.ncsa.illinois.edu", help="InfluxDB URL for logging")
+        self.parser.add_argument('--influxPort', dest="influx_port", type=int, nargs='?',
+                                 default=8086, help="InfluxDB port")
+        self.parser.add_argument('--influxUser', dest="influx_user", type=str, nargs='?',
+                                 default="terra", help="InfluxDB username")
+        self.parser.add_argument('--influxPass', dest="influx_pass", type=str, nargs='?',
+                                 default="", help="InfluxDB password")
+        self.parser.add_argument('--influxDB', dest="influx_db", type=str, nargs='?',
+                                 default="extractor_db", help="InfluxDB databast")
+
         # parse command line and load default logging configuration
         self.setup()
 
         # setup logging for the exctractor
         logging.getLogger('pyclowder').setLevel(logging.DEBUG)
         logging.getLogger('__main__').setLevel(logging.DEBUG)
+
+        # assign other arguments
+        self.output_dir = self.args.output_dir
+        self.force_overwrite = self.args.force_overwrite
+        self.influx_host = self.args.influx_host
+        self.influx_port = self.args.influx_port
+        self.influx_user = self.args.influx_user
+        self.influx_pass = self.args.influx_pass
+        self.influx_db = self.args.influx_db
 
     # Check whether dataset already has metadata
     def check_message(self, connector, host, secret_key, resource, parameters):
@@ -45,14 +71,18 @@ class Ply2LasConverter(Extractor):
             out_name = resource['name'] + " MergedPointCloud.las"
             out_las = os.path.join(out_dir, out_name)
 
-            if os.path.exists(out_las):
-                logging.info("...output file already exists; skipping %s" % resource['id'])
+            if os.path.exists(out_las) and not self.force_overwrite:
+                logging.info("...output LAS file already exists; skipping %s" % resource['id'])
             else:
                 return CheckMessage.download
 
         return CheckMessage.ignore
 
     def process_message(self, connector, host, secret_key, resource, parameters):
+        starttime = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
+        created = 0
+        bytes = 0
+
         east_ply = None
         west_ply = None
         for p in resource['local_paths']:
@@ -67,7 +97,7 @@ class Ply2LasConverter(Extractor):
         out_name = resource['name'] + " MergedPointCloud.las"
         out_las = os.path.join(out_dir, out_name)
 
-        if east_ply and west_ply:
+        if not os.path.exists(out_las) or self.force_overwrite:
             tmp_east_las = "east_temp.las"
             logging.info("...converting %s to %s" % (east_ply, tmp_east_las))
             subprocess.call(['pdal translate ' + \
@@ -88,15 +118,42 @@ class Ply2LasConverter(Extractor):
 
             logging.info("...merging into %s" % out_las)
             subprocess.call(['pdal merge '+tmp_east_las+' '+tmp_west_las+' '+out_las], shell=True)
-            if os.path.isfile(out_las):
+            if os.path.isfile(out_las) and out_las not in resource["local_paths"]:
                 # Send LAS output to Clowder source dataset
-                logging.info("uploading %s to dataset" % out_las)
-                pyclowder.files.upload_to_dataset(connector, host, secret_key, resource['parent']['id'], out_las)
+                fileid = pyclowder.files.upload_to_dataset(connector, host, secret_key, resource['parent']['id'], out_las)
+
+            created += 1
+            bytes += os.path.getsize(out_las)
 
             if os.path.exists(tmp_east_las):
                 os.remove(tmp_east_las)
             if os.path.exists(tmp_west_las):
                 os.remove(tmp_west_las)
+
+            endtime = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
+            self.logToInfluxDB(starttime, endtime, created, bytes)
+
+    def logToInfluxDB(self, starttime, endtime, filecount, bytecount):
+        # Time of the format "2017-02-10T16:09:57+00:00"
+        f_completed_ts = int(parse(endtime).strftime('%s'))
+        f_duration = f_completed_ts - int(parse(starttime).strftime('%s'))
+
+        client = InfluxDBClient(self.influx_host, self.influx_port, self.influx_user, self.influx_pass, self.influx_db)
+        client.write_points([{
+            "measurement": "file_processed",
+            "time": f_completed_ts,
+            "fields": {"value": f_duration}
+        }], tags={"extractor": self.extractor_info['name'], "type": "duration"})
+        client.write_points([{
+            "measurement": "file_processed",
+            "time": f_completed_ts,
+            "fields": {"value": int(filecount)}
+        }], tags={"extractor": self.extractor_info['name'], "type": "filecount"})
+        client.write_points([{
+            "measurement": "file_processed",
+            "time": f_completed_ts,
+            "fields": {"value": int(bytecount)}
+        }], tags={"extractor": self.extractor_info['name'], "type": "bytes"})
 
 if __name__ == "__main__":
     extractor = Ply2LasConverter()
