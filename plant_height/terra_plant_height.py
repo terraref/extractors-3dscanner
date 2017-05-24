63718e4c73ab7ca11931411d9a6b7210a2c77ada
Max Burnette <mburnet88@gmail.com>
2017-05-24 14:44:33 -0500

    Update plant_height for terrautils

--- plant_height/terra_plant_height.py
+++ plant_height/terra_plant_height.py
@@ -1,40 +1,31 @@
 #!/usr/bin/env python
 
+import datetime
 import os
 import logging
 import full_day_to_histogram
 import numpy as np
-from plyfile import PlyData, PlyElement
-
-import datetime
-from dateutil.parser import parse
-from influxdb import InfluxDBClient, SeriesHelper
 
 from pyclowder.extractors import Extractor
 from pyclowder.utils import CheckMessage
 import pyclowder.files
 import pyclowder.datasets
+import terrautils.extractors
 
+from plyfile import PlyData, PlyElement
 
-def determineOutputDirectory(outputRoot, dsname):
-    if dsname.find(" - ") > -1:
-        timestamp = dsname.split(" - ")[1]
-    else:
-        timestamp = "dsname"
-    if timestamp.find("__") > -1:
-        datestamp = timestamp.split("__")[0]
-    else:
-        datestamp = ""
-
-    return os.path.join(outputRoot, datestamp, timestamp)
 
 class Ply2HeightEstimation(Extractor):
     def __init__(self):
         Extractor.__init__(self)
 
+        influx_host = os.getenv("INFLUXDB_HOST", "terra-logging.ncsa.illinois.edu")
+        influx_port = os.getenv("INFLUXDB_PORT", 8086)
+        influx_db = os.getenv("INFLUXDB_DB", "extractor_db")
+        influx_user = os.getenv("INFLUXDB_USER", "terra")
+        influx_pass = os.getenv("INFLUXDB_PASSWORD", "")
+
         # add any additional arguments to parser
-        # self.parser.add_argument('--max', '-m', type=int, nargs='?', default=-1,
-        #                          help='maximum number (default=-1)')
         self.parser.add_argument('--output', '-o', dest="output_dir", type=str, nargs='?',
                                  default="/home/extractor/sites/ua-mac/Level_1/scanner3DTop_plant_height",
                                  help="root directory where timestamp & output directories will be created")
@@ -43,15 +34,15 @@ class Ply2HeightEstimation(Extractor):
         self.parser.add_argument('--dockerpdal', dest="pdal_docker", type=bool, nargs='?', default=False,
                                  help="whether PDAL should be run inside a docker container")
         self.parser.add_argument('--influxHost', dest="influx_host", type=str, nargs='?',
-                                 default="terra-logging.ncsa.illinois.edu", help="InfluxDB URL for logging")
+                                 default=influx_host, help="InfluxDB URL for logging")
         self.parser.add_argument('--influxPort', dest="influx_port", type=int, nargs='?',
-                                 default=8086, help="InfluxDB port")
+                                 default=influx_port, help="InfluxDB port")
         self.parser.add_argument('--influxUser', dest="influx_user", type=str, nargs='?',
-                                 default="terra", help="InfluxDB username")
+                                 default=influx_user, help="InfluxDB username")
         self.parser.add_argument('--influxPass', dest="influx_pass", type=str, nargs='?',
-                                 default="", help="InfluxDB password")
+                                 default=influx_pass, help="InfluxDB password")
         self.parser.add_argument('--influxDB', dest="influx_db", type=str, nargs='?',
-                                 default="extractor_db", help="InfluxDB databast")
+                                 default=influx_db, help="InfluxDB database")
 
         # parse command line and load default logging configuration
         self.setup()
@@ -64,14 +55,19 @@ class Ply2HeightEstimation(Extractor):
         self.output_dir = self.args.output_dir
         self.force_overwrite = self.args.force_overwrite
         self.pdal_docker = self.args.pdal_docker
-        self.influx_host = self.args.influx_host
-        self.influx_port = self.args.influx_port
-        self.influx_user = self.args.influx_user
-        self.influx_pass = self.args.influx_pass
-        self.influx_db = self.args.influx_db
+        self.influx_params = {
+            "host": self.args.influx_host,
+            "port": self.args.influx_port,
+            "db": self.args.influx_db,
+            "user": self.args.influx_user,
+            "pass": self.args.influx_pass
+        }
 
     # Check whether dataset already has metadata
     def check_message(self, connector, host, secret_key, resource, parameters):
+        if not terrautils.extractors.is_latest_file(resource):
+            return CheckMessage.ignore
+
         # Check if we have 2 PLY files, but not an LAS file already
         east_ply = None
         west_ply = None
@@ -83,10 +79,11 @@ class Ply2HeightEstimation(Extractor):
                     west_ply = p['filepath']
 
         if east_ply and west_ply:
-            out_dir = determineOutputDirectory(self.output_dir, resource['dataset_info']['name'])
-            out_hist = os.path.join(out_dir, resource['dataset_info']['name'] + " histogram.npy")
-            out_top = os.path.join(out_dir, resource['dataset_info']['name'] + " highest.npy")
-
+            out_dir = terrautils.extractors.get_output_directory(self.output_dir, resource['dataset_info']['name'])
+            out_hist = os.path.join(out_dir, terrautils.extractors.get_output_filename(
+                    resource['dataset_info']['name'], 'npy', opts=['histogram']))
+            out_top = os.path.join(out_dir, terrautils.extractors.get_output_filename(
+                    resource['dataset_info']['name'], 'npy', opts=['highest']))
             if not self.force_overwrite and os.path.isfile(out_hist) and os.path.isfile(out_top):
                 logging.info("...outputs already exist; skipping %s" % resource['id'])
             else:
@@ -101,34 +98,33 @@ class Ply2HeightEstimation(Extractor):
         uploaded_file_ids = []
 
         # Get left/right files and metadata
-        metafile, ply_east, ply_west, metadata = None, None, None, None
+        ply_east, ply_west, metadata = None, None, None
         for fname in resource['local_paths']:
             # First check metadata attached to dataset in Clowder for item of interest
             if fname.endswith('_dataset_metadata.json'):
                 all_dsmd = full_day_to_histogram.load_json(fname)
                 for curr_dsmd in all_dsmd:
                     if 'content' in curr_dsmd and 'lemnatec_measurement_metadata' in curr_dsmd['content']:
-                        metafile = fname
                         metadata = curr_dsmd['content']
             # Otherwise, check if metadata was uploaded as a .json file
-            elif fname.endswith('_metadata.json') and fname.find('/_metadata.json') == -1 and metafile is None:
-                metafile = fname
-                metadata = full_day_to_histogram.lower_keys(full_day_to_histogram.load_json(metafile))
+            elif fname.endswith('_metadata.json') and fname.find('/_metadata.json') == -1 and metadata is None:
+                metadata = full_day_to_histogram.lower_keys(full_day_to_histogram.load_json(fname))
             elif fname.endswith('-east_0.ply'):
                 ply_east = fname
             elif fname.endswith('-west_0.ply'):
                 ply_west = fname
-        if None in [metafile, ply_east, ply_west, metadata]:
+        if None in [ply_east, ply_west, metadata]:
             logging.error('could not find all 3 of east/west/metadata')
             return
 
         # Determine output locations
-        out_dir = determineOutputDirectory(self.output_dir, resource['dataset_info']['name'])
-        logging.info("output directory: %s" % out_dir)
+        out_dir = terrautils.extractors.get_output_directory(self.output_dir, resource['dataset_info']['name'])
         if not os.path.exists(out_dir):
             os.makedirs(out_dir)
-        out_hist = os.path.join(out_dir, resource['dataset_info']['name'] + " histogram.npy")
-        out_top = os.path.join(out_dir, resource['dataset_info']['name'] + " highest.npy")
+        out_hist = os.path.join(out_dir, terrautils.extractors.get_output_filename(
+                resource['dataset_info']['name'], 'npy', opts=['histogram']))
+        out_top = os.path.join(out_dir, terrautils.extractors.get_output_filename(
+                resource['dataset_info']['name'], 'npy', opts=['highest']))
 
         logging.info("Loading %s & calculating height information" % ply_west)
         plydata = PlyData.read(str(ply_west))
@@ -152,45 +148,13 @@ class Ply2HeightEstimation(Extractor):
                 uploaded_file_ids.append(fileid)
 
         # Tell Clowder this is completed so subsequent file updates don't daisy-chain
-        metadata = {
-            "@context": {
-                "@vocab": "https://clowder.ncsa.illinois.edu/clowder/assets/docs/api/index.html#!/files/uploadToDataset"
-            },
-            "dataset_id": resource['id'],
-            "content": {
-                "files_created": uploaded_file_ids
-            },
-            "agent": {
-                "@type": "cat:extractor",
-                "extractor_id": host + "/api/extractors/" + self.extractor_info['name']
-            }
-        }
+        metadata = terrautils.extractors.build_metadata(host, self.extractor_info['name'], resource['id'], {
+            "files_created": uploaded_file_ids}, 'dataset')
         pyclowder.datasets.upload_metadata(connector, host, secret_key, resource['id'], metadata)
 
         endtime = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
-        self.logToInfluxDB(starttime, endtime, created, bytes)
-
-    def logToInfluxDB(self, starttime, endtime, filecount, bytecount):
-        # Time of the format "2017-02-10T16:09:57+00:00"
-        f_completed_ts = int(parse(endtime).strftime('%s'))*1000000000
-        f_duration = f_completed_ts - int(parse(starttime).strftime('%s'))*1000000000
-
-        client = InfluxDBClient(self.influx_host, self.influx_port, self.influx_user, self.influx_pass, self.influx_db)
-        client.write_points([{
-            "measurement": "file_processed",
-            "time": f_completed_ts,
-            "fields": {"value": f_duration}
-        }], tags={"extractor": self.extractor_info['name'], "type": "duration"})
-        client.write_points([{
-            "measurement": "file_processed",
-            "time": f_completed_ts,
-            "fields": {"value": int(filecount)}
-        }], tags={"extractor": self.extractor_info['name'], "type": "filecount"})
-        client.write_points([{
-            "measurement": "file_processed",
-            "time": f_completed_ts,
-            "fields": {"value": int(bytecount)}
-        }], tags={"extractor": self.extractor_info['name'], "type": "bytes"})
+        terrautils.extractors.log_to_influxdb(self.extractor_info['name'], self.influx_params,
+                                              starttime, endtime, created, bytes)
 
 if __name__ == "__main__":
     extractor = Ply2HeightEstimation()
