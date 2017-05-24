3181f39faa3ff67ca1eff9f372c2e3ca0b688abf
Max Burnette <mburnet88@gmail.com>
2017-05-24 14:36:47 -0500

    Update for terrautils & Docker

--- ply2las/terra_ply2las.py
+++ ply2las/terra_ply2las.py
@@ -1,38 +1,29 @@
 #!/usr/bin/env python
 
-import os, shutil
-import logging
-import subprocess
-
 import datetime
-from dateutil.parser import parse
-from influxdb import InfluxDBClient, SeriesHelper
+import os
+import logging
+import shutil
+import subprocess
 
 from pyclowder.extractors import Extractor
 from pyclowder.utils import CheckMessage
 import pyclowder.files
 import pyclowder.datasets
+import terrautils.extractors
 
-
-def determineOutputDirectory(outputRoot, dsname):
-    if dsname.find(" - ") > -1:
-        timestamp = dsname.split(" - ")[1]
-    else:
-        timestamp = "dsname"
-    if timestamp.find("__") > -1:
-        datestamp = timestamp.split("__")[0]
-    else:
-        datestamp = ""
-
-    return os.path.join(outputRoot, datestamp, timestamp)
 
 class Ply2LasConverter(Extractor):
     def __init__(self):
         Extractor.__init__(self)
 
+        influx_host = os.getenv("INFLUXDB_HOST", "terra-logging.ncsa.illinois.edu")
+        influx_port = os.getenv("INFLUXDB_PORT", 8086)
+        influx_db = os.getenv("INFLUXDB_DB", "extractor_db")
+        influx_user = os.getenv("INFLUXDB_USER", "terra")
+        influx_pass = os.getenv("INFLUXDB_PASSWORD", "")
+
         # add any additional arguments to parser
-        # self.parser.add_argument('--max', '-m', type=int, nargs='?', default=-1,
-        #                          help='maximum number (default=-1)')
         self.parser.add_argument('--output', '-o', dest="output_dir", type=str, nargs='?',
                                  default="/home/extractor/sites/ua-mac/Level_1/scanner3DTop_mergedlas",
                                  help="root directory where timestamp & output directories will be created")
@@ -41,15 +32,15 @@ class Ply2LasConverter(Extractor):
         self.parser.add_argument('--dockerpdal', dest="pdal_docker", type=bool, nargs='?', default=False,
                                  help="whether PDAL should be run inside a docker container")
         self.parser.add_argument('--influxHost', dest="influx_host", type=str, nargs='?',
-                                 default="terra-logging.ncsa.illinois.edu", help="InfluxDB URL for logging")
+                                 default=influx_host, help="InfluxDB URL for logging")
         self.parser.add_argument('--influxPort', dest="influx_port", type=int, nargs='?',
-                                 default=8086, help="InfluxDB port")
+                                 default=influx_port, help="InfluxDB port")
         self.parser.add_argument('--influxUser', dest="influx_user", type=str, nargs='?',
-                                 default="terra", help="InfluxDB username")
+                                 default=influx_user, help="InfluxDB username")
         self.parser.add_argument('--influxPass', dest="influx_pass", type=str, nargs='?',
-                                 default="", help="InfluxDB password")
+                                 default=influx_pass, help="InfluxDB password")
         self.parser.add_argument('--influxDB', dest="influx_db", type=str, nargs='?',
-                                 default="extractor_db", help="InfluxDB databast")
+                                 default=influx_db, help="InfluxDB database")
 
         # parse command line and load default logging configuration
         self.setup()
@@ -62,14 +53,19 @@ class Ply2LasConverter(Extractor):
         self.output_dir = self.args.output_dir
         self.force_overwrite = self.args.force_overwrite
         self.pdal_docker = self.args.pdal_docker
-        self.influx_host = self.args.influx_host
-        self.influx_port = self.args.influx_port
-        self.influx_user = self.args.influx_user
-        self.influx_pass = self.args.influx_pass
-        self.influx_db = self.args.influx_db
+        self.influx_params = {
+            "host": self.args.influx_host,
+            "port": self.args.influx_port,
+            "db": self.args.influx_db,
+            "user": self.args.influx_user,
+            "pass": self.args.influx_pass
+        }
 
     # Check whether dataset already has metadata
     def check_message(self, connector, host, secret_key, resource, parameters):
+        if not terrautils.extractors.is_latest_file(resource):
+            return CheckMessage.ignore
+
         # Check if we have 2 PLY files, but not an LAS file already
         east_ply = None
         west_ply = None
@@ -81,10 +77,9 @@ class Ply2LasConverter(Extractor):
                     west_ply = p['filepath']
 
         if east_ply and west_ply:
-            out_dir = determineOutputDirectory(self.output_dir, resource['name'])
-            out_name = resource['name'] + " MergedPointCloud.las"
+            out_dir = terrautils.extractors.get_output_directory(self.output_dir, resource['name'])
+            out_name = terrautils.extractors.get_output_filename(resource['name'], 'las', opts=['merged'])
             out_las = os.path.join(out_dir, out_name)
-
             if os.path.exists(out_las) and not self.force_overwrite:
                 logging.info("output LAS file already exists; skipping %s" % resource['id'])
             else:
@@ -107,10 +102,10 @@ class Ply2LasConverter(Extractor):
                     west_ply = p
 
         # Create output in same directory as input, but check name
-        out_dir = determineOutputDirectory(self.output_dir, resource['name'])
+        out_dir = terrautils.extractors.get_output_directory(self.output_dir, resource['name'])
         if not os.path.exists(out_dir):
             os.makedirs(out_dir)
-        out_name = resource['name'] + " MergedPointCloud.las"
+        out_name = terrautils.extractors.get_output_filename(resource['name'], 'las', opts=['merged'])
         out_las = os.path.join(out_dir, out_name)
 
         if not os.path.exists(out_las) or self.force_overwrite:
