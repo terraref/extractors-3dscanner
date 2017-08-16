e687ef20a448e61339ce49ec26c5c93c31e2e4aa
Max Burnette <mburnet88@gmail.com>
2017-08-16 09:55:41 -0500

    Update metadata syntax

--- ply2las/terra_ply2las.py
+++ ply2las/terra_ply2las.py
@@ -128,7 +128,7 @@ class Ply2LasConverter(TerrarefExtractor):
                 os.remove(tmp_west_las)
 
             # Tell Clowder this is completed so subsequent file updates don't daisy-chain
-            metadata = build_metadata(host, self.extractor_info['name'], target_dsid, {
+            metadata = build_metadata(host, self.extractor_info, target_dsid, {
                 "files_created": [fileid]}, 'dataset')
             upload_metadata(connector, host, secret_key, target_dsid, metadata)
 
