22529c16860af444a63b279d2363af3b187130db
Jeff Terstriep <jefft@illinois.edu>
2018-02-20 23:34:46 +0000

    Modify Dockerfile to use Ubuntu packages instead of source build.
    Building opencv and PCL from scratch is complex. This uses the slightly
    older version of opencv and PCL that are distributed as Ubuntu packages.
    PCLDLL.h was modified to fix name changes of include files.

--- heightmap/PCLDLL.h
+++ heightmap/PCLDLL.h
@@ -1,9 +1,8 @@
 #include <opencv2/core/core.hpp>
 #include <opencv2/opencv.hpp>
 #include "opencv2/opencv_modules.hpp"
-#include <opencv2/core/utility.hpp>
-#include "opencv2/imgcodecs.hpp"
-#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
 
 #include <string>
 #include <iostream>
