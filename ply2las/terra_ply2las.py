bcad6e3f9a661540e844b84770e4b985d7d47738
Max Burnette <mburnet2@illinois.com>
2018-06-13 08:25:19 -0500

    run conversion in subprocess

--- ply2las/terra_ply2las.py
+++ ply2las/terra_ply2las.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 
 import os
-import logging
+import json
+import subprocess
 
 from pyclowder.utils import CheckMessage
 from pyclowder.files import upload_to_dataset
@@ -13,22 +14,12 @@ from terrautils.extractors import TerrarefExtractor, is_latest_file, \
 import terraref.laser3d
 
 
-def add_local_arguments(parser):
-    # add any additional arguments to parser
-    parser.add_argument('--dockerpdal', dest="pdal_docker", type=bool, nargs='?', default=False,
-                        help="whether PDAL should be run inside a docker container")
-
 class Ply2LasConverter(TerrarefExtractor):
     def __init__(self):
         super(Ply2LasConverter, self).__init__()
 
-        add_local_arguments(self.parser)
-
         # parse command line and load default logging configuration
         self.setup(sensor="laser3d_mergedlas")
-
-        # assign other arguments
-        self.pdal_docker = self.args.pdal_docker
 
     # Check whether dataset already has metadata
     def check_message(self, connector, host, secret_key, resource, parameters):
@@ -52,14 +43,14 @@ class Ply2LasConverter(TerrarefExtractor):
             timestamp = resource['dataset_info']['name'].split(" - ")[1]
             out_las = self.sensors.get_sensor_path(timestamp)
             if os.path.exists(out_las) and not self.overwrite:
-                logging.getLogger(__name__).info("output LAS file already exists; skipping %s" % resource['id'])
+                self.log_skip(resource, "output LAS file already exists")
             else:
                 return CheckMessage.download
 
         return CheckMessage.ignore
 
     def process_message(self, connector, host, secret_key, resource, parameters):
-        self.start_message()
+        self.start_message(resource)
         uploaded_file_ids = []
 
         east_ply = None
@@ -79,15 +70,22 @@ class Ply2LasConverter(TerrarefExtractor):
         out_las = self.sensors.create_sensor_path(timestamp)
 
         if not os.path.exists(out_las) or self.overwrite:
-            terraref.laser3d.generate_las_from_ply([east_ply, west_ply], out_las, 'east', terra_md)
+            self.log_info(resource, "Creating %s" % out_las)
+            self.execute_threaded_conversion([east_ply, west_ply], out_las, terra_md)
 
-            logging.getLogger(__name__).info("...created %s" % out_las)
             if os.path.isfile(out_las) and out_las not in resource["local_paths"]:
                 target_dsid = build_dataset_hierarchy(host, secret_key, self.clowder_user, self.clowder_pass, self.clowderspace,
                                                       self.sensors.get_display_name(),
                                                       timestamp[:4], timestamp[5:7],timestamp[8:10],
                                                       leaf_ds_name=self.sensors.get_display_name()+' - '+timestamp)
 
+                self.log_info(resource, "Uploading metadata")
+                # Upload original Lemnatec metadata to new Level_1 dataset
+                terra_md['raw_data_source'] = host + ("" if host.endswith("/") else "/") + "datasets/" + resource['id']
+                lemna_md = build_metadata(host, self.extractor_info, target_dsid, terra_md, 'dataset')
+                upload_metadata(connector, host, secret_key, target_dsid, lemna_md)
+
+                self.log_info(resource, "Uploading LAS file to Clowder")
                 # Send LAS output to Clowder source dataset
                 fileid = upload_to_dataset(connector, host, secret_key, target_dsid, out_las)
                 uploaded_file_ids.append(fileid)
@@ -100,12 +98,16 @@ class Ply2LasConverter(TerrarefExtractor):
                 "files_created": [host + ("" if host.endswith("/") else "/") + "files/" + fileid]}, 'dataset')
             upload_metadata(connector, host, secret_key, resource['id'], metadata)
 
-            # Upload original Lemnatec metadata to new Level_1 dataset
-            terra_md['raw_data_source'] = host + ("" if host.endswith("/") else "/") + "datasets/" + resource['id']
-            lemna_md = build_metadata(host, self.extractor_info, target_dsid, terra_md, 'dataset')
-            upload_metadata(connector, host, secret_key, target_dsid, lemna_md)
+            self.end_message(resource)
 
-            self.end_message()
+    def execute_threaded_conversion(self, ply_list, out_las, md):
+        with open("convert.py", 'w') as scriptfile:
+            scriptfile.write("import json\n")
+            scriptfile.write("import terraref.laser3d\n")
+            scriptfile.write("terraref.laser3d.generate_las_from_ply("+str(ply_list)+", '"+out_las+"', 'east', "+
+                             json.dumps(md).replace("true", "True")+")")
+
+        subprocess.call(["python convert.py"], shell=True)
 
 if __name__ == "__main__":
     extractor = Ply2LasConverter()
