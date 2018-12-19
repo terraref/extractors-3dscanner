4b6b6075712eae12f75512fd24103e57eb2001ad
Max Burnette <mburnet2@illinois.com>
2018-12-19 14:49:38 -0600

    las2height fixes

--- las2height/terra_las2height.py
+++ las2height/terra_las2height.py
@@ -7,10 +7,8 @@ import numpy as np
 from pyclowder.utils import CheckMessage
 from pyclowder.files import upload_to_dataset
 from pyclowder.datasets import upload_metadata, download_metadata, remove_metadata
-from terrautils.extractors import TerrarefExtractor, is_latest_file, create_image, \
-    build_metadata, calculate_gps_bounds, calculate_centroid, calculate_scan_time, \
-    build_dataset_hierarchy, geom_from_metadata, contains_required_files, file_exists, \
-    load_json_file, check_file_in_dataset
+from terrautils.extractors import TerrarefExtractor, is_latest_file, \
+    build_metadata, contains_required_files, file_exists, load_json_file, check_file_in_dataset
 from terrautils.metadata import get_terraref_metadata
 
 #TODO: from terraref.laser3d import las_to_height
@@ -33,43 +31,26 @@ class LAS2HeightEstimation(TerrarefExtractor):
             self.log_skip(resource, "missing required files")
             return CheckMessage.ignore
 
-        # Check metadata to verify we have what we need
-        md = download_metadata(connector, host, secret_key, resource['id'])
-        if get_terraref_metadata(md):
-            # Have TERRA-REF metadata, but not any from this extractor
-            return CheckMessage.download
-        else:
-            self.log_skip(resource, "no terraref metadata found")
-            return CheckMessage.ignore
+        return CheckMessage.download
 
     def process_message(self, connector, host, secret_key, resource, parameters):
-        self.start_message()
-        uploaded_file_ids = []
+        self.start_message(resource)
 
         # Load metadata from dataset
         las_file, terra_md_full = None, None
         for fname in resource['local_paths']:
             if fname.endswith('.las'):
                 las_file = fname
-            elif fname.endswith('_dataset_metadata.json'):
-                all_dsmd = load_json_file(fname)
-                terra_md_full = get_terraref_metadata(all_dsmd)
 
-        # Determine script name
-        target_scan = "unknown_scan"
-        if 'gantry_variable_metadata' in terra_md_full:
-            if 'script_name' in terra_md_full['gantry_variable_metadata']:
-                target_scan = terra_md_full['gantry_variable_metadata']['script_name']
-                if 'script_hash' in terra_md_full['gantry_variable_metadata']:
-                    target_scan += ' '+terra_md_full['gantry_variable_metadata']['script_hash']
+        target_scan = os.path.basename(las_file).replace("_merged.las", "")
 
         # Determine output locations
         plotname = resource['dataset_info']['name'].split(" - ")[1]
         date = resource['dataset_info']['name'].split(" - ")[2]
         out_hist = self.sensors.create_sensor_path(date, plot=plotname, filename=target_scan+"_histogram.csv")
+        uploaded_file_ids = []
 
-        hist = las_to_height_distribution(las_file, out_hist)
-        maximum = np.max(hist)
+        (hist, maximum) = las_to_height_distribution(las_file, out_hist)
         self.log_info(resource, "maximum height found: %s" % maximum)
 
         found_in_dest = check_file_in_dataset(connector, host, secret_key, resource['id'], out_hist, remove=self.overwrite)
@@ -83,26 +64,29 @@ class LAS2HeightEstimation(TerrarefExtractor):
 
         # Tell Clowder this is completed so subsequent file updates don't daisy-chain
         extractor_md = build_metadata(host, self.extractor_info, resource['id'], {
-            "files_created": uploaded_file_ids
+            "files_created": uploaded_file_ids,
+            "max_height": "%s" % maximum
         }, 'dataset')
         self.log_info(resource, "uploading extractor metadata to Level_1 dataset")
         remove_metadata(connector, host, secret_key, resource['id'], self.extractor_info['name'])
         upload_metadata(connector, host, secret_key, resource['id'], extractor_md)
 
-        self.end_message()
+        self.end_message(resource)
 
 def las_to_height_distribution(in_file, out_file):
     HIST_BIN_NUM = 500
-    zRange = [0, 5000] # depth scope of laser scanner, unit in mm
-    zOffset = 10  # bin width of histogram, 10 mm for each bin
-    scaleParam = 1000 # min unit in las might be 0.001 mm
+    zRange = [0, 5000]  # depth scope of laser scanner, unit in mm
+    zOffset = 10        # bin width of histogram, 10 mm for each bin
+    scaleParam = 1      # min unit in las might be 0.001 mm
     height_hist = np.zeros((zRange[1]-zRange[0])/zOffset)
 
     las_handle = File(in_file)
     zData = las_handle.Z
 
     if (zData.size) == 0:
-        return height_hist
+        return height_hist, 0
+
+    max_height = (np.max(zData)/100.0)
 
     with open(out_file, 'w') as out:
         out.write("bin,range,pts\n")
@@ -116,12 +100,13 @@ def las_to_height_distribution(in_file, out_file):
                 zIndex = np.where(zData>zmin)
             else:
                 zIndex = np.where((zData>zmin) & (zData<zmax))
+
             num = len(zIndex[0])
             height_hist[i] = num
 
-            out.write("%s,%s,%s\n" % (i, "%s-%s" % (zmin, zmax), num))
+            out.write("%s,%s,%s\n" % (i, "%s-%s" % (zmin/100.0, zmax/100.0), num))
 
-    return height_hist
+    return height_hist, max_height
 
 if __name__ == "__main__":
     extractor = LAS2HeightEstimation()
